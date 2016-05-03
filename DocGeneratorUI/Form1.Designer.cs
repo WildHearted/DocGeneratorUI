102fd24d4ad86666d192622bc0df85f5604a3e56
Ben van ven Berg <Ben.vandenBerg@za.didata.com>
2016-05-03 23:29:29 +0200

    EOD 2015-05-03

--- DocGeneratorUI/Form1.Designer.cs
+++ DocGeneratorUI/Form1.Designer.cs
@@ -28,12 +28,77 @@
 		/// </summary>
 		private void InitializeComponent()
 			{
-			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.No;
+			this.pictureBox1.Image = global::DocGeneratorUI.Properties.Resources.DocGenerator;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(81, 81);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			// 
+			// button1
+			// 
+			this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+			this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.button1.Location = new System.Drawing.Point(133, 57);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(194, 39);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Generate Documents";
+			this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AllowDrop = true;
+			this.label1.Cursor = System.Windows.Forms.Cursors.No;
+			this.label1.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+			this.label1.Location = new System.Drawing.Point(100, 12);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(387, 42);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "This is the User Inteface to the Document Generator, which makes it possible to e" +
+    "xecute the DocGenenrator Core DLL on a local computer.";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label1.UseCompatibleTextRendering = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+			this.ClientSize = new System.Drawing.Size(461, 137);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "DocGenerator User Interface";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
 			}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Label label1;
 		}
 	}
 
--- DocGeneratorUI/Form1.cs
+++ DocGeneratorUI/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DocGeneratorCore;
 
 namespace DocGeneratorUI
 	{
@@ -14,6 +15,25 @@ namespace DocGeneratorUI
 		public Form1()
 			{
 			InitializeComponent();
+			}
+
+		private void Form1_Load(object sender, EventArgs e)
+			{
+
+			}
+
+		private void button1_Click(object sender, EventArgs e)
+			{
+			Cursor.Current = Cursors.WaitCursor;
+
+			MessageBox.Show("Here we go, ready to run a cycle of the DocGeneratorCore Controller as soon as you click Ok!", 
+				"Ready to run the DocGenerator... message.", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+
+			// Declare the main controller object
+			MainController objMainController = new MainController();
+			objMainController.MainProcess();
+
+			Cursor.Current = Cursors.Default;
 			}
 		}
 	}
